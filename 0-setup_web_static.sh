a2190f076c5024869d07f46fe932b3ecfa9e76ef
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-02-01 11:19:40 +0300

    feat: prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# sets up your web servers for the deployment of web_static
+sudo apt -y update
+# Install Nginx if it not already installed
+command -v nginx || sudo apt install nginx
+
+# Create the folder /data/ if it doesn’t already exist
+mkdir -p /data/
+
+# Create the folder /data/web_static/ if it doesn’t already exist
+mkdir -p /data/web_static/
+
+# Create the folder /data/web_static/releases/ if it doesn’t already exist
+mkdir -p /data/web_static/releases/
+
+# Create the folder /data/web_static/shared/ if it doesn’t already exist
+mkdir -p /data/web_static/shared/
+
+# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+mkdir -p /data/web_static/releases/test/
+
+# Create a fake HTML file /data/web_static/releases/test/index.html
+FILE="/data/web_static/releases/test/index.html"
+mkdir -p "$(dirname "$FILE")"
+cat <<EOF > "$FILE"
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+EOF
+
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
+sudo rm -f /data/web_static/current && sudo ln -s /data/web_static/releases/test /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Define the path to the Nginx configuration file
+nginx_config_file="/etc/nginx/sites-available/default"
+
+# Define the path to the web_static directory
+web_static_dir="/data/web_static/current/"
+
+# Define the URL path for serving static content
+static_url_path="/hbnb_static"
+
+# Add alias directive to Nginx configuration
+sudo sed -i "/location \/ {/a \    alias $web_static_dir;" $nginx_config_file
+
+# Add location block for serving static content
+sudo sed -i "/location \/ {/a \    location $static_url_path { }" $nginx_config_file
+
+# Restart Nginx to apply changes
+sudo systemctl restart nginx
