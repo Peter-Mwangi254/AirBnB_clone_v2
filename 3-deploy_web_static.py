f63a988975ec37b66b294e2873712c6677acdb75
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-02-04 10:17:18 +0300

    feat: full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,14 +1,35 @@
 #!/usr/bin/python3
-"""Distributes an archive to your web servers, using the function do_deploy
+"""A Fabric script (based on the file 2-do_deploy_web_static.py) that
+creates and distributes an archive to your web servers
 """
 
 from os import path
-from fabric.api import put, env, run
+from datetime import datetime
+from fabric.api import put, env, run, local
 
 
 env.hosts = ['100.26.49.195', '54.152.70.222']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_pack():
+    """Generates a .tgz archive from the contents of
+    the web_static folder
+    """
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = 'versions/web_static_' + timestamp + '.tgz'
+
+    # create the versions folder if it doesn't exist
+    local("mkdir -p versions")
+
+    # create the .tgz archive
+    result = local(f"tar -cvzf {archive_path} web_static/")
+
+    if result.succeeded:
+        return archive_path
+    else:
+        return None
 
 
 def do_deploy(archive_path):
@@ -55,3 +76,13 @@ web_static_{}/ /data/web_static/current'.format(timestamp))
 
     # return True on success
     return True
+
+
+def deploy():
+    """Deploys web static
+    """
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    success = do_deploy(archive_path)
+    return success
