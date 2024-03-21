48df9fcf95cec27fd35ef245d3a7690f74156c05
Peter-Mwangi254 <pm6093628@gmail.com>
2024-03-21 13:53:54 -0700

    script that starts a Flask web application

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+
+"""Starts a Flask web application.
+That listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: display “C ” followed by the value of a text variable
+    /python/(<text>): Displays 'Python' followed by the value of <text>
+    /number/<n>: Displays 'n is a number' only if <n> is an integer
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hnbn():
+    """returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    """Displays 'C' followed by the value of <text>."""
+    return 'C ' + text.replace('_', ' ')
+
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """Displays 'Python' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
