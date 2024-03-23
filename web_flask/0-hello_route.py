8b20b0623382d7d1833a191cd68de5e7d4db6d62
Peter-Mwangi254 <pm6093628@gmail.com>
2024-03-23 00:31:29 -0700

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,9 +4,10 @@ start Flask application
 that listens on:
     0.0.0.0, port 5000
 """
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
