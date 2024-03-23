eae2e2868c3a7a76f753469faf04ecdf0150fb42
Peter-Mwangi254 <pm6093628@gmail.com>
2024-03-23 01:21:24 -0700

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -12,20 +12,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hnbn():
     """returns Hello HBNB!"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def cisfun(text):
     """Displays 'C' followed by the value of <text>."""
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
@@ -33,7 +37,8 @@ def pythoniscool(text='is cool'):
     """Displays 'Python' followed by the value of <text>.
     Replaces any underscores in <text> with slashes.
     """
-    return 'Python ' + text.replace('_', ' ') 
+    return 'Python ' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
